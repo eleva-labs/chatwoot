162c75d3a0ec266e01ae1445bb32f49b60c1fbf0
Juan Carlos Valverde Solera <valverdesolera@gmail.com>
2025-09-14 15:53:24 -0600

    Add Twilio SMS channel file size limit constant
    Introduces MAXIMUM_FILE_UPLOAD_SIZE_TWILIO_SMS_CHANNEL to define a 5MB file size limit for Twilio SMS channel uploads.

--- app/javascript/shared/constants/messages.js
+++ app/javascript/shared/constants/messages.js
@@ -37,6 +37,9 @@ export const CONVERSATION_PRIORITY_ORDER = {
 // Size in mega bytes
 export const MAXIMUM_FILE_UPLOAD_SIZE = 40;
 
+// Twilio SMS channel has a 5MB file size limit
+export const MAXIMUM_FILE_UPLOAD_SIZE_TWILIO_SMS_CHANNEL = 5;
+
 export const ALLOWED_FILE_TYPES =
   'image/*,' +
   'audio/*,' +
